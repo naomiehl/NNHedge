aaf36274c03591712d15598f5ffd1c7f692e01a8
Naomie Halioua <59069658+naomiehl@users.noreply.github.com>
2022-07-25 16:47:21 +0200

    Update train.py

--- NNHedge/_train/train.py
+++ NNHedge/_train/train.py
@@ -4,6 +4,7 @@ import numpy as np
 
 def single_epoch_train(model, optimizer, trainloader, loss_func, epoch, model_type:str, K=100):
     running_loss = 0.0 
+    
     if model_type not in ['RNN','TCN','ATTENTION','SpanMLP']:
         raise ValueError('Please use an available type of model. Available Models: RNN | TCN | ATTENTION |MLP')
 
@@ -15,13 +16,18 @@ def single_epoch_train(model, optimizer, trainloader, loss_func, epoch, model_ty
         C    = resolve_shape(C)
         Sn   = resolve_shape(Sn)
         Sn_1 = resolve_shape(Sn_1)
+        
+         # Zero the parameter gradients
         optimizer.zero_grad()
-
+        
+        # Forward + Backward + Optimize
         outputs = model(span)
 
         loss = loss_func(outputs * (Sn - Sn_1) - C + torch.max(Sn - K, torch.zeros(256, 1)), torch.zeros(256, 1))
         loss.backward()
         optimizer.step()
+        
+         # Print statistics
         running_loss += loss.item()
 
     print('[%d] loss: %.6f' % (epoch + 1, running_loss))   
